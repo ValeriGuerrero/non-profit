ab70547b47ff686733e5935aa4a6cb7039f19637
emayaisdope <emaya.nelson@gmail.com>
2022-11-19 11:37:09 -0600

    add error message

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
 
+const form = document.getElementById('form');
+const username = document.getElementById('firstname');
+const email = document.getElementById('email');
+const form_fields = [firstname, email];
+
+// Check required fields
+function checkRequired(inputArr) {
+    validated = true;
+    inputArr.forEach(function (input) {
+      if (input.value.trim() === '') {
+        showError(input, `${getFieldName(input)} is required.`);
+        validated = false;
+      }
+    });
+   return validated;
+  }
+
+  // Show input error message
+function showError(input, message) {
+    const formControl = input.parentElement;
+    formControl.className = 'form-control error';
+    const small = formControl.querySelector('small');
+    small.innerText = message;
+  }
