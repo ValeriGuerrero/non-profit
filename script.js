a9cdc1f87245a66a402d09dd5bad16857093c9b5
emayaisdope <emaya.nelson@gmail.com>
2022-11-18 18:27:56 -0600

    update thank you message

--- script.js
+++ script.js
@@ -1,10 +1,3 @@
-var nextStep = document.querySelector('#submit');
-
-nextStep.addEventListener('click', function (e) {
-  e.preventDefault();
-  // Hide first view
-  document.getElementById('form').style.display = 'none';
-
-  // Show thank you message element
-  document.getElementById('thank_you').style.display = 'block';
-});
+function thankYou() {
+    alert('Thank You!')
+}
